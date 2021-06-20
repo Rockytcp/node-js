98276be501469e27f5fc5413758c30ca5c3ceb35
“Rockytcp” <gustavo.braganti@gmail.com>
2021-06-19 21:12:27 -0300

    Adcionado data de Criacao e Data futura no banco de dados

--- infraestrutura/tabelas.js
+++ infraestrutura/tabelas.js
@@ -5,7 +5,7 @@ class Tabelas{
     }
     
     criarAtendimentos(){
-        const sql = "CREATE TABLE IF NOT EXISTS Atendimentos (id serial, cliente char(50) NOT NULL, pet char(20), servico char(20) NOT NULL, status char(20) NOT NULL, observacoes text, PRIMARY KEY(id))"
+        const sql = "CREATE TABLE IF NOT EXISTS Atendimentos (id serial, cliente char(50) NOT NULL, pet char(20), servico char(20) NOT NULL, data timestamp NOT NULL, dataCriacao timestamp NOT NULL, status char(20) NOT NULL, observacoes text, PRIMARY KEY(id))"
         this.conexao.query(sql, erro => {
             if(erro){
                 console.log(erro)
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,18 +1,25 @@
+const moment = require("moment")
 const conexao = require("../infraestrutura/conexao")
 
 
 class Atendimento{
     adciona(atendimento){
-        console.log(typeof(atendimento))
-        console.log(atendimento)
+        const dataCriacao = moment().format("YYYY-MM-DD hh:mm:ss")
+        const data = moment(atendimento.data, "DD/MM/YYYY").format("YYYY-MM-DD hh:mm:ss")
+        const atendimentoDatado = {...atendimento, dataCriacao, data}
+        console.log(typeof(atendimentoDatado))
+        console.log(atendimentoDatado)
         const values = []
-        let item
-        for(item in atendimento ){
-            values.push(atendimento[item])
-        }
+        values.push(atendimentoDatado["cliente"])
+        values.push(atendimentoDatado["pet"])
+        values.push(atendimentoDatado["servico"])
+        values.push(atendimentoDatado["data"])
+        values.push(atendimentoDatado["dataCriacao"])
+        values.push(atendimentoDatado["status"])
+        values.push(atendimentoDatado["observacoes"])
         console.log(typeof(values))
         console.log(values)
-        const sql = "INSERT INTO Atendimentos(cliente, pet, servico, status, observacoes) VALUES($1, $2, $3, $4, $5)"
+        const sql = "INSERT INTO Atendimentos(cliente, pet, servico, data, dataCriacao, status, observacoes) VALUES($1, $2, $3, $4, $5, $6, $7)"
 
         conexao.query(sql, values, (erro, resultados) => {
             if(erro){
